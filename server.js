18c684be088a40eaa601261eea7e8aee50c5c4f4
unknown <jaylesh27@gmail.com>
2017-12-12 16:48:54 -0500

    added boilerplate code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+	const webpackMiddleware = require("webpack-dev-middleware");
+	const webpack = require("webpack");
+	const webpackConfig = require("./webpack.config.js");
+	app.use(webpackMiddleware(webpack(webpackConfig)));
+	console.log("running webpack development server");
+} else {
+	app.use(express.static("dist"));
+}
+
+app.listen(process.env.PORT || 3000, () => console.log("listening on port"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,50 @@
-var wepback = require('webpack');
-var path = require('path');
-
+var webpack = require("webpack");
+var path = require("path");
 
 // This webpack plugin creates an index.html file (based off the template specified in our app directory) with script tags for the bundled JS files and will get outputted in the build directory.
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const VENDOR_LIBS = ["axios", "body-parser", "express", "react", "react-dom"];
+
+module.exports = {
+	entry: {
+		bundle: "./src/index.js",
+		vendor: VENDOR_LIBS
+	},
+
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "[name].[chunkhash].js"
+	},
+
+	module: {
+		rules: [
+			{
+				use: "babel-loader",
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				use: ["style-loader", "css-loader"],
+				test: /\.css$/
+			}
+		]
+	},
+
+	node: {
+		console: true,
+		fs: "empty",
+		net: "empty",
+		tls: "empty"
+	},
+
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ["vendor", "manifest"]
+		}),
+
+		new HtmlWebpackPlugin({
+			template: "src/index.html"
+		})
+	]
+};
