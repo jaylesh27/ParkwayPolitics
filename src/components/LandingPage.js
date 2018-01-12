6894ccb242a5b2faaffcb8881bca6d224d4c9057
unknown <jaylesh27@gmail.com>
2018-01-11 21:29:59 -0500

    added images and more styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import { slide as Menu } from 'react-burger-menu';
+import { bubble as Menu } from 'react-burger-menu';
 import logo from '../../assets/images/parkway-politics-logo.jpg';
-
+import soundcloud_icon from '../../assets/images/soundcloud-icon.png';
+import twitter_icon from '../../assets/images/twitter-icon.png';
+import facebook_icon from '../../assets/images/facebook-icon.png';
 
 var styles = {
   bmBurgerButton: {
@@ -32,7 +34,8 @@ var styles = {
   },
   bmItemList: {
     color: '#b8b7ad',
-    padding: '0.8em'
+    padding: '0.8em',
+    background: '#373a47'
   },
   bmOverlay: {
     background: 'rgba(0, 0, 0, 0.3)'
@@ -52,7 +55,10 @@ export default class LandingPage extends Component {
 			<div>
 				<Menu styles={ styles }>
 					<Link to="/">About us</Link>
-					<Link to="/">Podcasts</Link>
+					<Link to="/">Episodes</Link>
+					<a href="https://twitter.com/ParkwayPolitics" target="_blank"><img src={twitter_icon} alt="twitter icon" /></a>
+					<a href="https://facebook.com" target="_blank"><img src={facebook_icon} alt="facebook icon" /></a>
+					<a href="https://soundcloud.com/parkwaypolitics" target="_blank"><img src={soundcloud_icon} alt="soundcloud icon" /></a>
 					
 				</Menu>
 				<div id = "hero-section">
@@ -60,8 +66,8 @@ export default class LandingPage extends Component {
 						<div id = "intro-section">
 							<h1>Parkway Politics</h1>
 							<img src={logo} alt="parkway politics logo" />
-							<h4>Welcome to Parkway Politic's home on the web.</h4>
-							<p>Well?  Go on, explore the rest of the site and listen to our content!</p>
+							<h4>Welcome to Parkway Politic's home on the web</h4>
+							{/*<h5>Well?  Go on, explore the rest of the site and listen to our episodes!  Binge-listening podcsts > binge-watching Netflix</h5>*/}
 							
 						</div>
 					</div>
