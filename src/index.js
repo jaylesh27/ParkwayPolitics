49e5f581672ebc85559b67527bc52f77ed2215eb
unknown <jaylesh27@gmail.com>
2018-01-14 14:54:57 -0500

    added favicon, cleaned up some comments and scripts

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app.js';
 import '../assets/css/style.css';
+import '../assets/images/favicon.ico';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require("webpack");
 var path = require("path");
 
-// This webpack plugin creates an index.html file (based off the template specified in our app directory) with script tags for the bundled JS files and will get outputted in the build directory.
+// This webpack plugin creates an index.html file (based off the template specified in our src directory) with script tags for the bundled JS files and will get outputted in the build directory.
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // VENDOR_LIBS will contain an array of strings with each item in the array being the name of the 3rd party library we want to include in the separate vendor file
@@ -14,7 +14,7 @@ module.exports = {
 	},
 
 	output: {
-		path: path.join(__dirname, "dist"),
+		path: path.join(__dirname, "build"),
 		filename: "[name].[chunkhash].js"
 	},
 
@@ -33,11 +33,11 @@ module.exports = {
 				]
 			},
 			{
-				// below line checks for any files that are jpg, jpeg, png, gif, or svg files (regex)
-				test: /\.(png|jpg|gif)$/,
+				// below line checks for any files that are jpg, png, gif, or ico files (regex)
+				test: /\.(png|jpg|gif|ico)$/,
 				use: [
 					{
-						// the url-loader's purpose is to take the images inside of our project and copy them into our build folder.  When this is done, the url-loader goes back to the import statement inside of our image_viewer.js file.  url-loader assigns a new name to the image that surpasses the limit property below as a random set of characters.  this new name is then assigned to the big variable in the import statement
+						// the url-loader's purpose is to take the images inside of our project and copy them into our build folder.  When this is done, the url-loader goes back to the import statement inside of our files.  url-loader assigns a new name to the image that surpasses the limit property below as a random set of characters.  this new name is then assigned to the variable in the import statement
 						loader: 'url-loader',
 						// the options object has a limit property with a value of 40000.  this means look for any images that are greater than 40000 bytes, save it to the build/output directory.  If smaller, save it as raw data to bundle.js
 						options:  {
@@ -64,7 +64,8 @@ module.exports = {
 		}),
 
 		new HtmlWebpackPlugin({
-			template: "src/index.html"
+			template: "src/index.html",
+			favicon: "assets/images/favicon.ico"
 		})
 	]
 };
