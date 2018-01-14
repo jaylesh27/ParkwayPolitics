c8845b2ddccd69ac70415c9a56c94ad7d743d5ef
unknown <jaylesh27@gmail.com>
2018-01-13 22:47:23 -0500

    added content and styling

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 const AboutUs = () => {
 
 	return (
-		<div className="row">
+		<div className="row about-us-section">
+			<h3 className="center-align">Meet the (soon-to-be) illustrious hosts</h3>
+			<div className="col s12 m6" id="about-ron">
+				
+			</div>
+			<div className="col s12 m6" id="about-jay">
+			</div>
 		</div>
 	);
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -10,11 +10,11 @@ export default class LandingPage extends Component {
 	render() {
 		return(
 			<div>
-				<div id = "hero-section">
-					<div id = "hero-overlay">
-						<div id = "intro-section">
+				<div id="hero-section">
+					<div id="hero-overlay">
+						<div id="intro-section">
 							<h3>Welcome to Parkway Politics' home on the web</h3>
-							<img src={logo} alt="parkway politics logo" />
+							<img src={logo} alt="Parkway Politics logo" />
 							<h5>On this podcast, we talk about the news and issues affecting New Jersey</h5>
 							<h5>Check us out here:</h5>
 							<span id="social-media-links">
@@ -25,18 +25,17 @@ export default class LandingPage extends Component {
 						</div>
 					</div>
 				</div>
-
-				<div className="row">
-					<div className="col s6">
+				<div className="row podcast-info">
+					<div className="col s12 m6" id="fake-news-blurb">
 						<h3>So much fake news, so little time.</h3>
 						<p>But we do hope you'll make time for us.  Our podcast is dedicated to bringing New Jersey residents the news they need to stay informed in our current ten-news-cycles-a-day maelstrom.  Stick with us as we dig deep into what's happening in New Jersey and occasionally comment on what's happening at the national level because there's certainly a lot of crazy to dissect up there.</p>
 					</div>
-					<div className="col s6">
+					<div className="col s12 m6" id="latest-episode">
 						<h3>Listen to our latest episode</h3>
-						<iframe width="100%" height="300" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/382617281&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>
+						<iframe width="100%" height="300" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/382617281&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true">
+						</iframe>
 					</div>
 				</div>
-
 				<AboutUs />
 			</div>
 		);
