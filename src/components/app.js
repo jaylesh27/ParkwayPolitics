4ac447c8d62d9bd6083fdc492e38b4590f322dd8
unknown <jaylesh27@gmail.com>
2018-03-09 18:31:32 -0500

    encountered bugs with previous build, fixes included

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import LandingPage from './LandingPage';
-import AboutUs from './AboutUs';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default class App extends Component {
 
 	render() {
 		return (
-			<BrowserRouter>
-				<div>
-					<Switch>
-						<Route exact path='/' component={LandingPage} />
-						<Route path='/about-us' component={AboutUs}/>
-					</Switch>
-				</div>
-			</BrowserRouter>
+			<div>
+				<LandingPage />
+			</div>
 		);
 	}
 }
