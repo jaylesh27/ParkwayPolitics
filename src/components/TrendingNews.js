14b6777219f08751976a94946d7af3eba78f8773
unknown <jaylesh27@gmail.com>
2018-03-23 21:05:35 -0400

    small css changes and link fix

--- src/components/TrendingNews.js
+++ src/components/TrendingNews.js
@@ -51,7 +51,7 @@ class TrendingNews extends Component {
     render() {
         return(
             <div className="container">
-                <h4 className="center-align">Check out stories from <a href="https://www.nj.com/politics" target="_blank">nj.com</a>. Support local journalism!</h4>
+                <h4 className="center-align">Check out stories from <a href="http://www.nj.com/politics" target="_blank">nj.com</a>. Support local journalism!</h4>
                 <div className="row nj-news-articles">
                     <ArticleDetails newsNJ={this.state.newsNJ} />
                 </div>
