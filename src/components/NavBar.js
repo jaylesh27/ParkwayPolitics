99789c13a83cde82ef6a31502f641bdd90745cef
unknown <jaylesh27@gmail.com>
2018-03-25 20:38:20 -0400

    added episodes data in json and started work on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,13 @@ import React from 'react';
 const NavBar = () => {
 
 	return (
-		<div className="navbar-fixed">
+		<header>
 			<nav>
-				<div className="nav-wrapper">
-					<a href="#" className="brand-logo center">
-						Parkway Politics
-					</a>
-				</div>
+				<a href="#" className="brand-logo center">
+					Parkway Politics
+				</a>
 			</nav>
-		</div>
+		</header>
 	);
 };
 
