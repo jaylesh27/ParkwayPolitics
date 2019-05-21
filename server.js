906127a80b261e71dd9cde1082cf73d4242a1320
unknown <jaylesh27@gmail.com>
2019-05-20 21:27:07 -0400

    fixed news section issues

--- server.js
+++ server.js
@@ -44,18 +44,22 @@ app.get("/", function(req, res) {
 
 app.get('/api/fetch-nj-news', function(req, res) {
 
-	request('https://www.nj.com/politics', function(error, response, html) {
+	request('https://www.nj.com/politics/', function(error, response, html) {
 	console.log("fetch news error: "+error);
 		var $ = cheerio.load(html);
 		var results = [];
 
-		$('#river-container ul li.river-item.has-photo article').each(function(i, element) {
+		$('.river article.river-item.river-item--thumbnail').each(function(i, element) {
 
-			var articleLink = $(element).find('div.item-text').find('h2.h2.fullheadline').find('a').attr('href');
-			var articleImgLink = $(element).find('div.item-extra').find('div.item-photo').find('img').attr('src');
-			var articleHeadline = $(element).find('div.item-text').find('h2.h2.fullheadline').find('a').text();
+			var articleLink = $(element).find('h2').find('a').attr('href');
+			// console.log(articleLink);
+			var articleImgLink = $(element).children('a').children('img').attr('data-src');
+			// console.log(articleImgLink);
+			// var articleHeadline = $(element).find('div.item-text').find('h2.h2.fullheadline').find('a').text();
+			var articleHeadline = $(element).find('div.river-item__content').find('h2').find('a').text();
 			var trimmedHeadline = articleHeadline.replace("\n                        ", "");
 			var finalHeadline = trimmedHeadline.replace('\n                    ', "");
+			console.log(finalHeadline);
 			var articleId = $(element).parent().attr('data-resource-id');
 			results.push({ link: articleLink, image: articleImgLink, headline: finalHeadline, id: articleId });
 		});
